eab76c595d00fc643565897a5dc9c204d7d8da93
rodrigopdo <rodrigo.prudente82@gmail.com>
2025-10-22 23:58:12 -0300

    chore: initial project structure

---
+++ src/main/java/pdo/fraudshield/FraudshieldApplication.java
@@ -0,0 +1,13 @@
+package pdo.fraudshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudshieldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudshieldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pdo/fraudshield/FraudshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package pdo.fraudshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudshieldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
